7b2ff76c163b892e34f6347345653c1509a5d58b
ne4ajb <bogdanlitvin8@gmail.com>
2018-07-05 20:18:09 +0300

    [+] add test to Article

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:article) { create(:article) }
+
+  before(:each) do
+    sign_in user
+  end
+
+  describe 'GET #index' do
+    it 'renders the template with status' do
+      get :index
+      expect(response).to render_template(:index)
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'renders the template with status' do
+      get :show, params: { id: article.id }
+      expect(response).to render_template(:show)
+      expect(response.status).to eq(200)
+    end
+  end
+end
