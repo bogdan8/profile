d44340c6e712529b2f0e729504c1ad888cf117dc
ne4ajb <bogdanlitvin8@gmail.com>
2018-07-09 22:58:49 +0300

    [+] add new class to image

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,7 +1,7 @@
 module AttachmentsHelper
   # method for get image or video
   def get_attachment(attachment)
-    return image_tag(attachment.image) if attachment.image?
-    return video_tag(attachment.video) if attachment.video?
+    return image_tag(attachment.image, class: 'responsive') if attachment.image?
+    return video_tag(attachment.video, class: 'responsive') if attachment.video?
   end
 end
