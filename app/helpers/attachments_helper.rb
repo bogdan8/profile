c74ca263d00f8e916c3cfdb4d78ff8983ea284d1
ne4ajb <bogdanlitvin8@gmail.com>
2018-07-10 21:22:21 +0300

    [~] refactor code

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -2,6 +2,6 @@ module AttachmentsHelper
   # method for get image or video
   def get_attachment(attachment)
     return image_tag(attachment.image, class: 'responsive') if attachment.image?
-    return video_tag(attachment.video, class: 'responsive') if attachment.video?
+    return video_tag(attachment.video, class: 'responsive', controls: true) if attachment.video?
   end
 end
