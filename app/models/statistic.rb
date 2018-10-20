234528be0f07ee596069526f330af38b2a578893
ne4ajb <bogdanlitvin8@gmail.com>
2018-10-20 18:57:53 +0300

    [+] add annotation

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -7,6 +7,8 @@
 #  id         :bigint(8)        not null, primary key
 #  ip_address :string
 #  browser    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 
 class Statistic < ApplicationRecord
