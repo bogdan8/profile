7f0ca495f46d4702d913c3f2408ef4bf8c5af2af
ne4ajb <bogdanlitvin8@gmail.com>
2018-10-20 17:28:15 +0300

    [~] rollback database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019172246) do
+ActiveRecord::Schema.define(version: 20180914181445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,11 +83,6 @@ ActiveRecord::Schema.define(version: 20181019172246) do
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
-  end
-
-  create_table "statistics", force: :cascade do |t|
-    t.string "ip_address"
-    t.string "browser"
   end
 
   create_table "users", force: :cascade do |t|
