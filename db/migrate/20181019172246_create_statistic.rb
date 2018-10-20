d13786099a130b44cd549ed8136e018ceb35de81
ne4ajb <bogdanlitvin8@gmail.com>
2018-10-20 17:29:13 +0300

    [+] add timestamp to Statistic

--- db/migrate/20181019172246_create_statistic.rb
+++ db/migrate/20181019172246_create_statistic.rb
@@ -3,6 +3,8 @@ class CreateStatistic < ActiveRecord::Migration[5.1]
     create_table :statistics do |t|
       t.string :ip_address
       t.string :browser
+
+      t.timestamps
     end
   end
 end
