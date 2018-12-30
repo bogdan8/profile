d59266b84d20adfdcccc9432e8f7a8a45c1891fe
ne4ajb <bogdanlitvin8@gmail.com>
2018-12-30 10:40:09 +0200

    [~] fix paperclip erros after upgrade rails

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -3,7 +3,7 @@
 module AttachmentsHelper
   # method for get image or video
   def get_attachment(attachment)
-    return image_tag(attachment.image, class: 'responsive') if attachment.image?
-    return video_tag(attachment.video, class: 'responsive', controls: true) if attachment.video?
+    return image_tag(attachment.image.url, class: 'responsive') if attachment.image?
+    return video_tag(attachment.video.url, class: 'responsive', controls: true) if attachment.video?
   end
 end
