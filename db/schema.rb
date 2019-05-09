b19747e1d38c59c5271936b53c7e662b2475cba2
ne4ajb <bogdanlitvin8@gmail.com>
2019-05-09 18:02:18 +0300

    [+] add category books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_102150) do
+ActiveRecord::Schema.define(version: 2019_05_09_145309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2019_04_06_102150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position", default: 0
+    t.bigint "category_book_id"
+    t.index ["category_book_id"], name: "index_books_on_category_book_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -72,6 +74,10 @@ ActiveRecord::Schema.define(version: 2019_04_06_102150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id", "category_id"], name: "index_categorizations_on_article_id_and_category_id"
+  end
+
+  create_table "category_books", force: :cascade do |t|
+    t.string "title"
   end
 
   create_table "contacts", force: :cascade do |t|
--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -13,6 +13,8 @@
 #  src                :string
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
+#  position           :integer          default(0)
+#  category_book_id   :bigint(8)
 #
 
 FactoryBot.define do
